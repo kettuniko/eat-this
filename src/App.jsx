463e5fbffaeb289dfb518ac11122c9709e966023
Niko Kettunen <niko.kettunen@reaktor.fi>
2018-09-14 17:33:41 +0300

    Move player config to json

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
-import { compose, map, prop } from 'ramda'
+import React, { Fragment } from 'react'
+import { map } from 'ramda'
 import { Player } from './Player.jsx'
+import players from './players'
 
-export const App = compose(
-  map(player => <Player key={player.name} {...player} />),
-  prop('players')
+export const App = () => (
+  <Fragment>
+    {map(
+      player => (
+        <Player key={player.name} {...player} />
+      ),
+      players
+    )}
+  </Fragment>
 )
--- src/main.js
+++ src/main.js
@@ -3,23 +3,4 @@ import ReactDOM from 'react-dom'
 
 import { App } from './App.jsx'
 
-const players = [
-  {
-    name: 'Player 1',
-    keySet: {
-      A: '2',
-      B: '3',
-      LEFT: 'A'
-    }
-  },
-  {
-    name: 'Player 2',
-    keySet: {
-      A: '7',
-      B: '8',
-      LEFT: 'J'
-    }
-  }
-]
-
-ReactDOM.render(<App players={players} />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'))
